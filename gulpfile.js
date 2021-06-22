377f4ca9ed3e84d4b02aacd7d51a83674342dc96
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-21 23:42:44 -0500

    Estilos contenido festival

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass');
 // Funcion de compilacion de SASS
 function css() {
     return src('./src/scss/app.scss')
-            .pipe(sass({outputStyle: ''}))
+            .pipe(sass({outputStyle: 'compressed'}))
             .pipe(dest('./build/css'));
 }
 
@@ -16,3 +16,4 @@ function watchArchivos() {
 exports.css = css;
 exports.watchArchivos = watchArchivos;
 
+ 
