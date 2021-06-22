e60b6232686fcec845d69b713bf37d640a2e9791
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-22 08:49:05 -0500

    Minificando imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
 
 // Funcion de compilacion de SASS
 function css() {
@@ -8,12 +9,19 @@ function css() {
             .pipe(dest('./build/css'));
 }
 
+function imagenes() {
+    return src('./src/img/**/*')
+            .pipe(imagemin())
+            .pipe(dest('./build/img'));
+}
+
 function watchArchivos() {
     watch('./src/scss/**/*.scss', css)
 }
 
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 
  
