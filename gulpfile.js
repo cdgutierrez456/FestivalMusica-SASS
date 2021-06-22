5974449f3564297d92d8f515a8993578015f169c
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-22 09:37:43 -0500

    Convirtiendo imagenes a version .webp con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,42 @@ const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
 const notify = require('gulp-notify');
+const webp = require('gulp-webp');
 
 // Funcion de compilacion de SASS
+
+const paths = {
+    imagenes: './src/img/**/*',
+    scss: './src/scss/**/*.scss'
+}
+
 function css() {
-    return src('./src/scss/app.scss')
+    return src(paths.scss)
             .pipe(sass({outputStyle: 'compressed'}))
             .pipe(dest('./build/css'));
 }
 
 function imagenes() {
-    return src('./src/img/**/*')
+    return src(paths.imagenes)
             .pipe(imagemin())
             .pipe(dest('./build/img'))
             .pipe(notify({message: 'Imagen minificada'}));
 }
 
+function versionWebp() {
+    return src(paths.imagenes)
+            .pipe(webp())
+            .pipe(dest('./build/img'))
+            .pipe(notify({message: 'Imagen v.webp ok'}));
+}
+
 function watchArchivos() {
-    watch('./src/scss/**/*.scss', css)
+    watch(paths.scss, css)
 }
 
 
 exports.css = css;
 exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
-exports.default = series(css, imagenes, watchArchivos);
+exports.default = series(css, imagenes, versionWebp, watchArchivos);
  
