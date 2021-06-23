fd5740e54b65580503d82b973b7cd35b3d5c6b38
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-22 23:10:33 -0500

    Configurando scroll a secciones determinadas con enlaces superiores

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,5 +1,32 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
 
+    navegacionFija();
+
+});
+
+function navegacionFija() {
+    
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    
+    enlaces.forEach(function(enlace) {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({
+                behavior: 'smooth'
+            });
+
+        });
+    });
+
+}
 
 
 document.addEventListener('DOMContentLoaded', function() {
